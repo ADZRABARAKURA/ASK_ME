377f37df8c5836f0ef1dc9da681ee71ca4d187b3
NikitaShagvaliev <71658521+NikitaShagvaliev@users.noreply.github.com>
2024-04-18 21:57:09 +0500

    Создана страница авторизации
    Создана страница авторизации

---
+++ auth_page/Auth_Page.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./Auth.css";
+import { AuthForm } from "@/pages/auth_page/components/AuthForm/AuthForm";
+import { RegForm } from "@/pages/auth_page/components/RegForm/RegForm";
+
+export const Auth: React.FC = () => {
+  const [authReg, setAuthReg] = useState<boolean>(false);
+
+  const handleSwitchAuthReg = () => {
+    setAuthReg(!authReg);
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="main">
+        <img className="main__angel" src="angel.png" alt="angel.png" />
+        <div className="reg">
+          <div className="reg__left left-reg">
+            <div className="left-reg__choose">
+              <div
+                className={`left-reg__registation ${
+                  authReg ? "" : "active-btn"
+                }`}
+                onClick={handleSwitchAuthReg}
+              >
+                Регистрация
+              </div>
+              <div
+                className={`left-reg__auth ${authReg ? "active-btn" : ""}`}
+                onClick={handleSwitchAuthReg}
+              >
+                Авторизация
+              </div>
+            </div>
+            <div className="left-reg__welcome">Добро пожаловать</div>
+            {authReg ? <AuthForm /> : <RegForm />}
+          </div>
+          <div className="reg__right">
+            <img src="woman.jpg" alt="woman.jpg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
